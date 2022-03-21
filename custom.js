55a46f339718cee1eb4ca3bae925935b078ed614
ernestol1977 <ernestolondero@gmail.com>
2022-03-21 19:24:11 -0300

    retoque entrega Storage

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 // Creo la clase
 class Libro {
     constructor(titulo, autor, editorial, edicion, ano, numeroPaginas, generoLiterario, precio) {
+        // this.isbn = isbn; agregar resto
         this.titulo = titulo;
         this.autor = autor;
         this.editorial = editorial;
@@ -32,20 +33,33 @@ function cargaStorage (listaStorage) {
 }
 
 // Eliminar libro uno por uno
-function eliminarLibro(titulo) {
-    let traerStorage = capturarStorage();
-    let nuevoStorage = [];
-    for (const libro of traerStorage) {
-        if(libro.titulo == titulo) {
-            continue;
-        }
-        nuevoStorage.push(libro);
-    }
-    cargaStorage(nuevoStorage);
-}
+// function eliminarLibro(titulo) {
+//     let traerStorage = capturarStorage();
+//     let nuevoStorage = [];
+//     for (const libro of traerStorage) {
+//         if(libro.titulo == titulo) {
+//             continue;
+//         }
+//         nuevoStorage.push(libro);
+//     }
+//     cargaStorage(nuevoStorage);
+// }
 
+// Guardar libro
 function guardarNuevoLibro(libro){
     let listaStorage = capturarStorage()
     listaStorage.unshift(libro)
     cargaStorage(listaStorage)
 }
+
+function eliminarDeAUno(libros) {
+    let index = libros.findIndex((libro) => libro.titulo === libros.titulo);
+    libros.splice(index, 1);
+
+    if (libros.lenght === 0) {
+        libros = [];
+    }
+
+    localStorage.setItem("librosEnStorage", JSON.stringify(listaStorage));
+    crearString(libros);
+} 
--- dom.js
+++ dom.js
@@ -37,17 +37,21 @@ function crearString() {
         <div class="col">${libro.precio}</div>
         
         <div class="col">
-            <button class="btn btnModificar"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg></button>
-            <button class="btnEliminar btn btn-outline-dark" type="submit">Eliminar</button>
+            <button class="btn btnModificar">modificar</button>
+
+            <button id="${libro.titulo}" class="btn btn-outline-dark" type="submit">Eliminar</button>
         </div>
     </div>
     `
+    // let btnEliminar = document.getElementById(`${libro.titulo}`)
+    // btnEliminar.addEventListener("click", () =>
+    // eliminarDeAUno()
+    // )
     }
-
+    
     return string
 }
+
 
 function actualizarPantalla() {
     let cuerpo = document.getElementById("mostrar");
@@ -81,22 +85,10 @@ btnborrar.addEventListener("click", () => {
     libros = [];
 })
 
-// Definir botón borrar todo
+// Definir botón Actualizar
 let btnActualizar = document.getElementById("Actualizar");
 btnActualizar.addEventListener("click", () => {
 
     actualizarPantalla()
 })
 
-let eliminarBtn = document.querySelectorAll(".btnEliminar"); // traigo los botones "eliminar"
-eliminarBtn.forEach((item) => { //por cada boton de que cada card se ejecuta la funcion
-    item.addEventListener("click", (e) => {
-        eliminarBtn = e.target // le pregunto donde se hizo el evento
-        let libroDeHtml = eliminarBtn.closest(".libro"); //buscame el libro mas cerca de donde se hizo el evento
-        let nombreLibro = libroDeHtml.querySelector(".tituloLibro").textContent // busco los datos en base del libro que agarre antes
-        eliminarLibro(nombreLibro)
-
-        actualizarPantalla()
-    })
-
-})
